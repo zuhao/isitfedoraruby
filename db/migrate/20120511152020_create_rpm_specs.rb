6849092dc77c9203eeaa5a6cd07bc6719b7d2eec
Zuhao Wan <wanzuhao@gmail.com>
2012-05-11 23:39:28 +0800

    add inital models for GemSpec, RpmSpec and Comment

--- db/migrate/20120511152020_create_rpm_specs.rb
+++ db/migrate/20120511152020_create_rpm_specs.rb
@@ -2,11 +2,15 @@ class CreateRpmSpecs < ActiveRecord::Migration
   def change
     create_table :rpm_specs do |t|
       t.string :name, :null => false 
-      t.string :description, :fedorapkg, :type, :rpm_version
-      t.string :patch_version, :patch_summary
+      t.string :description
+      t.string :fedorapkg
+      t.string :type
+      t.string :rpm_version
+      t.string :patch_version
+      t.string :patch_summary
       t.integer :comments_count => {:default => 0}
       t.integer :works_count => {:default => 0}
-      t.references :gems => {:default => nil}
+      t.references :gem_specs => {:default => nil}
       t.timestamps
     end
   end
