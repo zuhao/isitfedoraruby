bed0fd6558ae3d49b6d451aca4e677c17c5eb1b1
Zuhao Wan <wanzuhao@gmail.com>
2012-05-24 01:07:43 +0800

    Retrieve all rpm info in JSON from Fedora PackageDB

--- app/models/rpm_importer.rb
+++ app/models/rpm_importer.rb
@@ -4,11 +4,13 @@ class RpmImporter
 
   FEDORAPKG_URI = 'https://admin.fedoraproject.org'
   QUERY = '/pkgdb/acls/list/?searchwords=rubygem-*&packages_tgp_limit=-1'
+  SUFFIX = 'tg_format=json'
 
   def self.import 
   	URI.parse(FEDORAPKG_URI + QUERY).read.scan(/\/pkgdb\/acls\/name\/rubygem-.+\?/).each do |rpm_uri|
-      rpm_uri = FEDORAPKG_URI + rpm_uri.chomp('?')
-      puts rpm_uri
+      rpm_uri = FEDORAPKG_URI + rpm_uri + SUFFIX
+      rpm_spec = ActiveSupport::JSON.decode(URI.parse(rpm_uri).read)
+      RpmSpec.new_from_rpm_tuple(rpm_spec)
   	end
   rescue Exception => ex 
   	puts ex.message
