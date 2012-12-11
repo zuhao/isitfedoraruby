95c9eb11af0c5ef8b760a6597b69a6a7e72078f0
nicolassatragno <nsatragno@gnome.org>
2012-12-11 16:35:38 -0300

    Added some text to translate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
-    @page_title = 'Welcome'
+    @page_title = _('Welcome')
     @popular_gems = RubyGem.most_popular.limit(10)
     @popular_rpms = FedoraRpm.most_popular.limit(10)
     @wanted_gems = RubyGem.most_wanted.limit(10)
