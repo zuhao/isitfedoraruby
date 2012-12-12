14f2bdd793caad9c034b5472459dc0255610519d
nicolassatragno <nsatragno@gnome.org>
2012-12-11 22:49:09 -0300

    Added po files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include FastGettext::Translation
-  before_filter :set_users_locale
-
-  def set_users_locale
-    I18n.locale = FastGettext.set_locale(params[:locale] || cookies[:locale] ||
-      request.env['HTTP_ACCEPT_LANGUAGE'] || 'en')
-    cookies[:locale] = I18n.locale if cookies[:locale] != I18n.locale.to_s
-  end
-
+  before_filter :set_gettext_locale
 end
--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,10 +1,10 @@
 # Define the text domain
-FastGettext.add_text_domain 'app', :path => File.join(File.dirname(__FILE__), '..', '..', 'locale')
+FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
  
+
+# set available locales
+# (note: the first one is used as a fallback if you try to set an unavailable locale)
+FastGettext.default_available_locales = ["en", "es"]
  
 # Set the default textdomain
 FastGettext.default_text_domain = 'app'
- 
-# set available locales
-# (note: the first one is used as a fallback if you try to set an unavailable locale)
-FastGettext.default_available_locales = ["en","es","cs","de","ar"]
