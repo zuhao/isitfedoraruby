576affba57fbcefc3cd60dac0e6fe31492fbffb0
Mark Klein <markpklein@gmail.com>
2012-12-19 07:38:14 -1000

    Improve Search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,21 +3,7 @@ class SearchesController < ApplicationController
   def index
     @page_title = 'Search'
     searchphrase = params[:search]
-	query = <<-QUERY 
-		CASE WHEN name like ? THEN 0
-        WHEN name like ? THEN 1
-        WHEN name like ? THEN 2 
-        ELSE 3 END, name
-        QUERY
-    sanitized_order = ActiveRecord::Base.send :sanitize_sql_array, [query,searchphrase+'%','% %' + searchphrase + '% %', '%'+searchphrase]
-    @results = RubyGem.where(
-        'name LIKE ?', "%#{searchphrase}%"
-    ).paginate(
-        :page => params[:page], 
-        :per_page => 50, 
-        :group => "name", 
-        :order => sanitized_order
-    )
+    @results = RubyGem.where('name LIKE ?', "%#{searchphrase}%").paginate(:page => params[:page], :per_page => 50, :group => "name", :order => "length(name) asc")
   end
   
 end
