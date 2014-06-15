ee1dc9cadf338bbe1fe985a0a7e6fe1a5d8b5359
Achilleas Pipinellis <axilleas@axilleas.me>
2014-06-15 18:51:24 +0300

    Raplce with rails_helper

--- spec/views/static_pages/contribute.html.haml_spec.rb
+++ spec/views/static_pages/contribute.html.haml_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "static_pages/contribute.html.haml" do
   pending "add some examples to (or delete) #{__FILE__}"
--- spec/views/static_pages/successes.html.haml_spec.rb
+++ spec/views/static_pages/successes.html.haml_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "static_pages/successes.html.haml" do
   pending "add some examples to (or delete) #{__FILE__}"
